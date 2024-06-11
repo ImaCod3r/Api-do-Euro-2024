ba3a6fc6d46156c848c97e13e78d0bc10e978ade
Edson Rodrigues <thecodeverse12@gmail.com>
2024-06-11 21:15:44 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from settings import *
+import os
 
-api = Flask(__name__)
+app = Flask(__name__)
 
-routes_config(api)
+routes_config(app)
 
-api.run(debug=True, port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
