656a8105d4fab4ecf5df4777be4a960cde984b9d
Edson Rodrigues <thecodeverse12@gmail.com>
2024-06-11 13:59:47 +0100

    Adding cors

--- settings.py
+++ settings.py
@@ -1,8 +1,10 @@
+from flask_cors import CORS
 from routes.matches import matches
 from routes.home import home 
 from routes.docs import docs
 
 def routes_config(api):
+    CORS(api)
     api.register_blueprint(home)
     api.register_blueprint(matches)
     api.register_blueprint(docs)
